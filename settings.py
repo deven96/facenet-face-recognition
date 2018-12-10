4be843c3fba7a699ebfdf0f2777f81c7500139ad
Deven96 <diretnandomnan@gmail.com>
2018-12-10 02:36:20 +0100

    froze model and moved loss function to fr_utils

--- settings.py
+++ settings.py
@@ -19,6 +19,7 @@ GLOB_IMAGE_DATABASE = "images/*"
 IMAGE_EXAMPLE_DIR = os.path.join(CURRENT_DIR, "images", "examples")
 
 WEIGHTS_DIR = os.path.join(CURRENT_DIR, "ai_handler", "weights")
+FROZEN_MODEL = os.path.join(CURRENT_DIR, "ai_handler", "frozen_model", "facenet.h5")
 MODEL_INPUT_SHAPE = (3, 96, 96)
 MIN_DISTANCE = 0.52 #defines the minimum distance used by the triplet loss function
 
